66b91091344dc71d89f10c9d9ef358c9aef75d56
tomo <rhbgp7kt@gmail.com>
2023-03-24 22:09:57 +0900

    一度押したボタンを使えなくなるように設定

--- main.js
+++ main.js
@@ -5,14 +5,16 @@
     const btn2 = document.getElementById('btn2');
     const btn3 = document.getElementById('btn3');
 
-    
+    // 合計の数字が入る
     var p = document.querySelector('p');
     var coin = 0;
     
 
+    //　ボタンをクリックしたら数字が追加される
     btn1.addEventListener('click', () => {
         coin++;
         p.textContent = coin;
+        btn1.classList.add('disabled');
     });
 
     btn2.addEventListener('click', () => {
